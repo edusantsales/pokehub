c9fb7c241b4dd45047e9208b95e396838507eb17
Eduardo Sales <eduardo.sales@compasso.com.br>
2024-03-28 11:42:19 -0300

    refactor: Renomeado a variável maxWidth para maxSize e atribuído uma largura e altura no seu valor

--- lib/src/application/extensions/build_context_extension.dart
+++ lib/src/application/extensions/build_context_extension.dart
@@ -5,7 +5,7 @@ import '../themes/palettes.dart';
 import '../themes/typographies.dart';
 
 extension BuildContextExtension on BuildContext {
-  Size get maxWidth => const Size.fromWidth(1440);
+  Size get maxSize => const Size(1440, 1080);
   NavigatorState get navigator => Navigator.of(this);
   Palettes get palettes => Theme.of(this).extension<Palettes>()!;
   ResponsiveService get responsive => ResponsiveService(this);
