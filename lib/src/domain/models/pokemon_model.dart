5997b5d421d986842701bb3ec0e88baccc575b78
Eduardo Sales <eduardo.sales@compasso.com.br>
2024-05-28 19:26:15 -0300

    refactor: Ajustes para melhorar a leitura de código

--- lib/src/domain/models/pokemon_model.dart
+++ lib/src/domain/models/pokemon_model.dart
@@ -2,8 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
-import '../../application/extensions/string_extension.dart';
-
 part 'pokemon_model.map.dart';
 
 @immutable
@@ -20,45 +18,7 @@ class PokemonModel {
 
   factory PokemonModel.fromJson(String source) => fromMap(json.decode(source) as Map<String, dynamic>);
 
-  final int? _id;
-  final String _name;
-  final List<String>? _sprites;
-  final List<String>? _types;
-
-  int get id => _id ?? 0;
-  String get name => _name;
-  List<String> get sprites => _sprites ?? <String>[];
-  List<String> get types => _types?.toSet().toList() ?? <String>[];
-  String get officialArtwork => sprites.isNotEmpty ? sprites.elementAt(0) : '';
-  String get showdown => sprites.isNotEmpty ? sprites.elementAt(1) : '';
-  String get primaryType => types.isNotEmpty ? types.first : '';
-  String get secondaryType => types.isNotEmpty ? types.last : '';
-  Color get primaryColor => _getColorByType(primaryType);
-  Color get secondaryColor => _getColorByType(secondaryType);
-
-  Color _getColorByType(String type) {
-    final Map<String, Color?> colors = <String, Color?>{
-      'Bug': const Color(0xFFC0CA33),
-      'Dark': const Color(0xFF424242),
-      'Dragon': const Color(0xFF283593),
-      'Electric': const Color(0xFFFFC107),
-      'Fairy': const Color(0xFFFF80AB),
-      'Fighting': const Color(0xFFB71C1C),
-      'Fire': const Color(0xFFFF9800),
-      'Flying': const Color(0xFF9FA8DA),
-      'Ghost': const Color(0xFF5C6BC0),
-      'Grass': const Color(0xFF4CAF50),
-      'Ground': const Color(0xFFE65100),
-      'Ice': const Color(0xFF4CD1C0),
-      'Normal': const Color(0xFF9E9E9E),
-      'Poison': const Color(0xFF9C27B0),
-      'Psychic': const Color(0xFFFF6675),
-      'Rock': const Color(0xFFC8B686),
-      'Steel': const Color(0xFF607D8B),
-      'Water': const Color(0xFF2196F3),
-    };
-    return colors[type.capitalize] ?? Colors.white10;
-  }
+  String toJson() => json.encode(toMap(this));
 
   PokemonModel copyWith({
     int? id,
@@ -74,10 +34,52 @@ class PokemonModel {
     );
   }
 
-  String toJson() => json.encode(toMap(this));
-
   @override
   String toString() {
     return toMap(this).toString();
+  }
+
+  final int? _id;
+  final String _name;
+  final List<String>? _sprites;
+  final List<String>? _types;
+
+  int get id => _id ?? 0;
+  String get name => _name;
+  List<String> get sprites => _sprites ?? <String>[];
+  List<String> get types => _getTypesDistinct();
+  String get officialArtwork => sprites.first;
+  String get showdown => sprites.last;
+  String get primaryType => types.first;
+  String get secondaryType => types.last;
+  Color get primaryColor => _getColorByType(primaryType);
+  Color get secondaryColor => _getColorByType(secondaryType);
+
+  List<String> _getTypesDistinct() {
+    return _types?.toSet().toList() ?? <String>[];
+  }
+
+  Color _getColorByType(String type) {
+    final Map<String, Color?> colors = <String, Color?>{
+      'bug': const Color(0xFFC0CA33),
+      'dark': const Color(0xFF424242),
+      'dragon': const Color(0xFF283593),
+      'electric': const Color(0xFFFFC107),
+      'fairy': const Color(0xFFFF80AB),
+      'fighting': const Color(0xFFB71C1C),
+      'fire': const Color(0xFFFF9800),
+      'flying': const Color(0xFF9FA8DA),
+      'ghost': const Color(0xFF5C6BC0),
+      'grass': const Color(0xFF4CAF50),
+      'ground': const Color(0xFFE65100),
+      'ice': const Color(0xFF4CD1C0),
+      'normal': const Color(0xFF9E9E9E),
+      'poison': const Color(0xFF9C27B0),
+      'psychic': const Color(0xFFFF6675),
+      'rock': const Color(0xFFC8B686),
+      'steel': const Color(0xFF607D8B),
+      'water': const Color(0xFF2196F3),
+    };
+    return colors[type] ?? Colors.white10;
   }
 }
